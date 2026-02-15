a75df21a8d894003276f1fda6f6f434732e8c09e
MSufyan <codewithsufi05@gmail.com>
2026-02-15 02:55:16 -0800

    updated scripts for windows and added vercel config

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import { ENV_VARS } from "./envVars.js";
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
-        // Ye line add karein taake confirm ho jaye
+       
         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error("❌ Error connecting to MONGODB: " + error.message);
--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,10 @@ import { connectDB } from "./config/db.js";
 import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
+const PORT = ENV_VARS.PORT
 const __dirname = path.resolve();
 
-// CORS ko simple rakhen Vercel ke liye
+
 app.use(cors({ 
     origin: true, 
     credentials: true 
@@ -24,7 +25,7 @@ app.use(cors({
 app.use(express.json()); 
 app.use(cookieParser());
 
-// Database connection ko route se pehle call karein (Serverless behavior)
+
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
@@ -32,18 +33,18 @@ app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
-// Vercel deployment ke liye static files handling
-if (process.env.NODE_ENV === "production") {
+
+if (ENV_VARS.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
 
-// Vercel ke liye app ko export karna zaruri hai
+
 export default app;
 
-// Local testing ke liye listen rehne dein
+
 if (process.env.NODE_ENV !== "production") {
     const PORT = ENV_VARS.PORT || 5000;
     app.listen(PORT, () => {
